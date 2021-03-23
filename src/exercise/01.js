fbd28ff1808e328f433cf0645fe688b496b37282
Jonathan Hsu <jonhsu.design@gmail.com>
2021-03-23 15:17:23 -0700

    finished ex1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,10 +3,12 @@
 
 import * as React from 'react'
 // 💣 remove this import
-import Globe from '../globe'
+// import Globe from '../globe'
 
 // 🐨 use React.lazy to create a Globe component which uses a dynamic import
 // to get the Globe component from the '../globe' module.
+const loadGlobe = () => import(/* webpackPrefetch: true */ '../globe')
+const Globe = React.lazy(loadGlobe)
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -26,17 +28,19 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
-        <input
-          type="checkbox"
-          checked={showGlobe}
-          onChange={e => setShowGlobe(e.target.checked)}
-        />
-        {' show globe'}
-      </label>
-      <div style={{width: 400, height: 400}}>
-        {showGlobe ? <Globe /> : null}
-      </div>
+      <React.Suspense fallback={<div>Loading.the globe..</div>}>
+        <label style={{marginBottom: '1rem'}} onMouseEnter={loadGlobe} onFocus={loadGlobe}>
+          <input
+            type="checkbox"
+            checked={showGlobe}
+            onChange={e => setShowGlobe(e.target.checked)}
+          />
+          {' show globe'}
+        </label>
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <Globe /> : null}
+        </div>
+      </React.Suspense>
     </div>
   )
 }
